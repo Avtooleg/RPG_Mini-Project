861d5ca08221fce83abca823b2c0f2d788ee8102
Олег Орлов <oleg.orlovic@gmail.com>
2021-11-20 09:41:57 +0300

    Corrected some small errors in Utils.h in get_random_int()

--- Utils.h
+++ Utils.h
@@ -4,6 +4,11 @@
 
 std::mt19937 mersenne(time(0));
 
+struct position{
+    int x;
+    int y;
+};
+
 int sign(float x){
     return (x>=0) - (x<0);
 }
@@ -16,7 +21,7 @@ int get_random_int(int min, int max){
     static const unsigned int mersenne_max = ~0;
     unsigned int initial = mersenne();
     static const double fraction = 1.0 / (static_cast<double>(mersenne_max) + 1.0);
-    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+    return static_cast<int>(initial * fraction * (max - min + 1) + min);
 };
 
 #ifndef RPG_MINIPROJECT_RANDOMIZER_H
