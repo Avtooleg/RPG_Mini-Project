25cd0d91fbd7c0dfad50c0ae096f8afdc706d6e1
GORRO45 <79827265+GORRO45@users.noreply.github.com>
2021-11-26 23:13:45 +0300

    Add files via upload

---
+++ Enemy.hpp
@@ -0,0 +1,28 @@
+#include "Creature.h"
+#include "Frontend.h"
+#include <vector>
+#include "Map.h"
+
+class Enemy:public ICreature
+{
+public:
+
+    void chase()
+    void roam()
+    
+    
+    position get_pos();
+
+
+private:
+    int xp;
+   
+};
+
+#ifndef Enemy_hpp
+#define Enemy_hpp
+
+#include <stdio.h>
+
+#endif /* Enemy_hpp */
+ 
